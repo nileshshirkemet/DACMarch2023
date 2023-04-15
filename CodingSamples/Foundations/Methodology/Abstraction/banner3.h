c3119411cdfe54b1c49b539cd12627aa5c51985a
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2023-04-15 11:59:31 +0530

    Add files via upload

---
+++ CodingSamples/Foundations/Methodology/Abstraction/banner3.h
@@ -0,0 +1,69 @@
+#include <cstdio>
+
+enum BorderStyle {None, Thin, Thick};
+
+class Banner
+{
+public:
+	//optional parameters of functions are initialized with default arguments
+	//which are implicitly passed when the corresponding arguments are not
+	//passed by the caller
+	Banner(float w = 20,  float h = 8)
+	{
+		width = w;
+		height = h;
+		border = BorderStyle::None;
+		puts("Banner instance initialized.");
+	}
+
+	void ApplyBorder(bool yes = true)
+	{
+		border = yes ? BorderStyle::Thin : BorderStyle::None; 
+	}
+
+	void ApplyBorder(BorderStyle style)
+	{
+		border = style;
+	}
+
+	void Resize(float w, float h) 
+	{
+		width = w;
+		height = h;
+	}
+
+	double Area() const
+	{
+		if(width < height)
+			throw width; //throw statement exits the function by
+						 //transferring control into the exception
+					     //handler with matching type
+		float t;
+		switch(border)
+		{
+			case BorderStyle::Thin:
+				t = 1;
+				break;
+			case BorderStyle::Thick:
+				t = 3;
+				break;
+			default:
+				t = 0;
+		}
+		return (width + 2 * t) * (height + 2 * t);
+	}
+
+	//A destructor is a special member function implemented to remove
+	//any side sffect of the constructor. It is automatically called 
+	//just before object is removed from the memory or its direct 
+	//identifier goes out of scope.
+	~Banner()
+	{
+		puts("Banner instance finalized.");
+	}
+private:
+	float width;
+	float height;
+	BorderStyle border;
+};
+
---
+++ CodingSamples/Foundations/Methodology/Abstraction/referencetest.cpp
@@ -0,0 +1,53 @@
+#include "banner3.h"
+#include <cstdio>
+
+/*
+A reference type (T&) identifier implicitly addresses one variable of
+matching type(T) in the initializer and automatically indirects to the
+value of that variable. In a function, accepting an argument of an object
+type through a const qualified parameter of corresponding reference type
+is logically equivalent to accepting the argument by value but without
+requiring construction and destruction of a copy of the instance passed
+in that argument.
+*/
+
+double BannerPrice(const Banner& info, int count)
+{
+	if(count < 10)
+		throw count;
+	float rate = count < 50 ? 0.80 : 0.75;
+	return rate * count * info.Area();
+}
+
+int main(void)
+{
+	Banner mybanner;
+	float w, h;
+	int n;
+
+	printf("Dimensions of your Banner: ");
+	scanf("%f%f", &w, &h);
+	printf("Number of Banners: ");
+	scanf("%d", &n);
+
+	Banner yourbanner(w, h);
+	yourbanner.ApplyBorder(); 
+
+	try
+	{
+		printf("Total price for my banners = %.2lf\n", BannerPrice(mybanner, n));
+		printf("Total price for your banners = %.2lf\n", BannerPrice(yourbanner, n));
+		yourbanner.ApplyBorder(BorderStyle::Thick);
+		printf("Total price for your banners with thick border = %.2lf\n", BannerPrice(yourbanner, n));
+	}
+	catch(int e)
+	{
+		printf("Error code: %d\n", e);
+	}
+	catch(float f)
+	{
+		printf("Invalid width: %f\n", f);
+	}
+
+}
+
