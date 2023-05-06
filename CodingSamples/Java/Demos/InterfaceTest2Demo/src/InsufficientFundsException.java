ff91f7aa625c8a83dccea7f504db03814b337000
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2023-05-06 18:49:16 +0530

    Delete CodingSamples/Java/Demos directory

--- CodingSamples/Java/Demos/InterfaceTest2Demo/src/InsufficientFundsException.java
+++
@@ -1,8 +0,0 @@
-package banking;
-
-//an exception whose class extends java.lang.Exception is checked
-//at compile time so that a method in which this exception occurs
-//reports it either by catching it (using try block) or by
-//declaring it to be thrown (using throws clause)
-public class InsufficientFundsException extends Exception {}
-
