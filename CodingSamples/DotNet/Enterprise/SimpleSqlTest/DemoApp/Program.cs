71fc3199c27ce122c2396821587888366dc1b04f
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2023-05-31 17:59:20 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Enterprise/SimpleSqlTest/DemoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using var connection = new Microsoft.Data.Sqlite.SqliteConnection();
+connection.ConnectionString = "Data Source=shop.db";
+connection.Open();
+using var command = connection.CreateCommand();
+if(args.Length == 0)
+{
+    command.CommandText = "SELECT ProductNo, Price, Stock FROM ProductInfo";
+    using var reader = command.ExecuteReader();
+    while(reader.Read())
+        Console.WriteLine("{0, -6}{1, 12:0.00}{2, 8}", reader.GetInt32(0), reader.GetDecimal(1), reader.GetInt32(2));
+}
+else
+{
+    int pno = int.Parse(args[0]);
+    command.CommandText = $"UPDATE ProductInfo SET Stock=Stock+5 WHERE ProductNo={pno}";
+    int n = command.ExecuteNonQuery();
+    if(n == 0)
+        Console.WriteLine("No such product!");
+}
