7753e140e50cf8f707351e50776c3ec35cd57818
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2023-06-28 18:37:14 +0530

    Add files via upload

---
+++ CodingSamples/JigarSirWeb/day3 - 28-62023/external.js
@@ -0,0 +1,9 @@
+function myfunction() {
+    //  alert("hello")
+    console.log(document);
+    console.log(document.getElementById('xyz'));
+    console.log(document.getElementById('abc'));
+
+    document.getElementById('xyz').style.backgroundColor = "red"
+
+}
